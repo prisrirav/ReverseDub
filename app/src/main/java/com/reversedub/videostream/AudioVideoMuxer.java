5f8d3e3a55ef14620c96bd9f5fe88201fc939a04
Lakshmi Priya Sekar <lasekar@microsoft.com>
2015-08-08 18:36:54 -0700

    Remove MediaMuxer method, use MP4Parser for muxing.

--- app/src/main/java/com/reversedub/videostream/AudioVideoMuxer.java
+++ app/src/main/java/com/reversedub/videostream/AudioVideoMuxer.java
@@ -1,114 +1,22 @@
 package com.reversedub.videostream;
 
-import android.content.res.AssetFileDescriptor;
-import android.media.MediaCodec.BufferInfo;
-import android.media.MediaExtractor;
-import android.media.MediaFormat;
-import android.media.MediaMuxer;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
 import com.coremedia.iso.boxes.Container;
 import com.googlecode.mp4parser.authoring.Movie;
 import com.googlecode.mp4parser.authoring.Track;
 import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
 import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
 
-import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.Buffer;
 import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
 import java.nio.channels.WritableByteChannel;
 
 public class AudioVideoMuxer  {
 
-    // Doesn't work, debugging in progress
-    private Boolean CombineFilesUsingMediaMuxer(String videoFile, String audioFile, String outputFile)
-    {
-        MediaMuxer muxer;
-        try
-        {
-            muxer = new MediaMuxer(outputFile, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-            return false;
-        }
-
-        MediaExtractor mediaVideoExtractor = new MediaExtractor();
-        try
-        {
-            mediaVideoExtractor.setDataSource(videoFile);
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-            return false;
-        }
-
-        MediaFormat videoFormat = mediaVideoExtractor.getTrackFormat(0);
-        MediaExtractor mediaAudioExtractor = new MediaExtractor();
-        try
-        {
-            mediaAudioExtractor.setDataSource(audioFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        MediaFormat audioFormat = mediaAudioExtractor.getTrackFormat(0);
-
-        int audioTrackIndex = muxer.addTrack(audioFormat);
-        int videoTrackIndex = muxer.addTrack(videoFormat);
-        ByteBuffer inputBuffer = ByteBuffer.allocate(1000000);
-        boolean finished = false;
-        boolean isAudioSample = true;
-        BufferInfo bufferInfo = new BufferInfo();
-
-        muxer.start();
-        while(!finished) {
-
-            if (isAudioSample)
-            {
-                bufferInfo.offset = 0;
-                bufferInfo.size = mediaAudioExtractor.readSampleData(inputBuffer, 0);
-                bufferInfo.flags = mediaAudioExtractor.getSampleFlags();
-                bufferInfo.presentationTimeUs = mediaAudioExtractor.getSampleTime();
-            }
-            else
-            {
-                bufferInfo.offset = 0;
-                bufferInfo.size = mediaVideoExtractor.readSampleData(inputBuffer, 0);
-                bufferInfo.flags = mediaVideoExtractor.getSampleFlags();
-                bufferInfo.presentationTimeUs = mediaVideoExtractor.getSampleTime();
-            }
-
-            if (bufferInfo.size < 0)
-            {
-                finished = true;
-                bufferInfo.size = 0;
-            }
-
-            if (!finished) {
-                int currentTrackIndex = isAudioSample ? audioTrackIndex : videoTrackIndex;
-                muxer.writeSampleData(currentTrackIndex, inputBuffer, bufferInfo);
-                isAudioSample = !isAudioSample;
-            }
-        };
-        muxer.stop();
-        muxer.release();
-        return true;
-    }
-
-    public static Boolean CombineFilesUsingMp4Parser(String videoFile, String audioFile, String outputFile)
+    public static Boolean Combine(String videoFile, String audioFile, String outputFile)
     {
         Movie video;
         try {
